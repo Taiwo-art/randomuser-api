9d8fcb290378846852630752ba902bb456b2c837
Taiwo Lambo <lmb.taiwo@gmail.com>
2022-09-04 22:06:53 +0100

    code corrected

--- main.js
+++ main.js
@@ -3,19 +3,16 @@ const randomUser = async() => {
     let jsonFetchUser = await fetchUser.json()
     let userDetails = jsonFetchUser.results
     
-    let outputUser = userDetails.map(person => 
-    `<img src ="${person.picture.large}">
-    <br> Full name: ${person.name.title} ${person.name.first} ${person.name.last} 
-    <br> Home: ${person.location.street.number}, ${person.location.street.name} 
-    <br> State: ${person.location.state} 
-    <br> Country: ${person.location.country} 
-    <br> E-mail: ${person.email} 
-    <br> DOB: ${person.dob.date}
-    <br> Age: ${person.dob.age}
-    <br> Phone: ${person.phone}`)     
+    let outputUser = `<img src="${userDetails[0].picture.large}">
+    <br> Full name: ${userDetails[0].name.title} ${userDetails[0].name.first} ${userDetails[0].name.last} 
+    <br> Home: ${userDetails[0].location.street.number}, ${userDetails[0].location.street.name} 
+    <br> State: ${userDetails[0].location.state} 
+    <br> Country: ${userDetails[0].location.country} 
+    <br> E-mail: ${userDetails[0].email} 
+    <br> DOB: ${userDetails[0].dob.date}
+    <br> Age: ${userDetails[0].dob.age}
+    <br> Phone: ${userDetails[0].phone}`   
  
     return document.body.innerHTML = outputUser
 }
-
 randomUser()
-
